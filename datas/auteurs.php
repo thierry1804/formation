6e910e18a71d2ac53f2d65b2fce537d55ff608a1
thierry1804 <thierry1804@gmail.com>
2019-05-12 12:09:25 +0300

    Ajout datas

---
+++ datas/auteurs.php
@@ -0,0 +1,24 @@
+<?php
+
+$auteurs = array(
+    'deepak' => array(
+        'nom' => 'Deepak Chopra',
+        'bibliographie' => "Auteur de best-sellers traduits en trente-cinq langues, parmi lesquels Comptent connaître Dieu, Les clés spirituelles de la richesse et Dieux de lumière, le Dr Deepak Chopra s'inspire de l'exceptionnelle sagesse des traditions spirituelles mondiales. Directeur de L'Alliance pour une nouvelle humanité, il enseigne partout dans le monde les clés d'une vie réussie.",
+    ),
+    'miguel' => array(
+        'nom' => 'Miguel Ruiz',
+        'bibliographie' => "Don Miguel Ruiz, bien que né et élevé au Mexique par une mère curandera (guérisseuse) et un grand-père nagual (chaman), choisit de faire des études de médecine et de devenir chirurgien. Une rencontre avec la mort (NDE), au début des années 70, a changé sa vie. Il s'est dès lors consacré à la maîtrise de la sagesse ancestrale. Il est maintenant devenu un nagual de la lignée des Chevaliers de l'Aigle, voué au partage de sa connaissance des enseignements des anciens Toltèques.",
+    ),
+    'eckhart' => array(
+        'nom' => 'Eckhart Tolle',
+        'bibliographie' => "Eckhart Tolle. Enseignant spirituel et auteur. il anime des séminaires et des retraites à travers le monde. Traduit en 33 langues. Le pouvoir du moment présent est devenu un best-seller par le simple bouche-à-oreille.",
+    ),
+    'wayne' => array(
+        'nom' => 'Wayne-W Dyer',
+        'bibliographie' => "Docteur en psychologie et psychothérapeute, le Dr Wayne W. Dyer a publié de nombreux best-sellers. Ecrivain et conférencier de renommée internationale, il a enseigné des principes psychologiques et spirituels fondamentaux.",
+    ),
+    'olivier' => array(
+        'nom' => 'Olivier Norek',
+        'bibliographie' => "Olivier Norek, lieutenant de police à la section enquêtes et recherches du SDPJ 93 depuis dix-sept ans, est l’auteur de Code 93 et de Territoires, deux polars largement salués par la critique.",
+    ),
+);
---
+++ datas/livres.php
@@ -0,0 +1,60 @@
+<?php
+
+$livres = array(
+    array(
+        'auteur' => 'deepak',
+        'titre' => "Les clés spirituelles de la richesse",
+        'resume' => "En se référant à la philosophie indienne et aux découvertes récentes de la physique quantique, le Dr Deepak Chopra détaille une série de principes psychologiques et spirituels qui visent à nous connecter à l'intelligence de l'univers. Plus nous sommes conscients des lois qui régissent l'univers, moins il nous faut d'efforts pour accéder à la richesse matérielle. Les désirs se réalisent alors spontanément, de façon quasi miraculeuse. Un guide clair et puissant pour une existence comblée.",
+        'isbn' => '2-290-33887-7',
+        'pochette' => '9782290338872-475x500-1.jpg',
+        'pages' => 123,
+        'ean' => '9782290338872',
+        'photo_auteur' => 'deepak.jpg'
+    ),
+    array(
+        'auteur' => 'miguel',
+        'titre' => "Les quatre accords toltèques - La voie de la liberté personnelle",
+        'resume' => "\"A chaque fois que vous le lisez, ce n'est pas le livre qui a changé, mais vous.\" Don Miguel Ruiz. Castaneda a fait découvrir au grand public les enseignements des chamans mexicains qui ont pour origine la tradition toltèque, gardienne des connaissances de Quetzacoatl, le serpent à plumes. Dans ce livre, Don Miguel révèle la source des croyances limitatrices qui nous privent de joie et créent des souffrances inutiles.
+Il montre en des termes très simples comment on peut se libérer du conditionnement collectif - le \"rêve de la planète\", basé sur la peur - afin de retrouver la dimension d'amour inconditionnel qui est à notre origine et constitue le fondement des enseignements toltèques. Les Quatre Accords proposent un puissant code de conduite capable de transformer rapidement notre vie en une expérience de liberté, de vrai bonheur et d'amour.
+Le monde fascinant de la Connaissance véritable et incarnée est enfin à la portée de chacun.",
+        'isbn' => '978-2-88911-654-6',
+        'pochette' => '9782889116546-475x500-1.jpg',
+        'pages' => 141,
+        'ean' => '9782889116546',
+        'video' => 'livre2.mp4',
+        'audio' => 'livre2.mp3',
+        'photo_auteur' => 'DomMiguelruiz.jpg'
+    ),
+    array(
+        'auteur' => 'eckhart',
+        'titre' => "L'art du calme intérieur - A l'écoute de sa nature essentiel",
+        'resume' => "Pour Eckhart Tolle, le calme ne consiste pas en une simple absence de bruit. \"Il est l'essence de toutes les galaxies et de tous les brins d'herbe; de toutes les fleurs, de tous les arbres, de tous les oiseaux et de toutes les autres formes.\" En accédant à ce calme intérieur, nous nous alignons sur le pouvoir et l'intelligence de la vie elle-même. Dans cet état, il n'y a ni souffrance, ni peur, juste une source d'amour, de joie et de paix intensément vivante.<br/>
+Ecrit sous une forme propice à la méditation, L'art du calme intérieur nous connecte, quelque soit le moment de la journée, à notre essence en nous rappelant qui nous sommes et ce qui compte réellement.",
+        'isbn' => '978-2-290-03675-4',
+        'pochette' => '9782290036754-475x500-1.jpg',
+        'pages' => 120,
+        'ean' => '9782290036754',
+        'photo_auteur' => 'EckhartTolle.jpg'
+    ),
+    array(
+        'auteur' => 'wayne',
+        'titre' => "Le pouvoir de l'intention - Apprendre à co-créer le monde à votre façon",
+        'resume' => "Selon Wayne W. Dyer, un des grands sages de notre époque, il existe un champ d'énergie pure et illimitée... l'intention. Lorsque nous sommes connectés à l'intention, une harmonie règne en nous et autour de nous ; on se sent inspiré et heureux, nos projets se réalisent, les relations deviennent sereines, les événements s'agencent en notre faveur. En nous connectant et en activant cette source, nous pouvons transformer nos existences de façon miraculeuse.<br/>
+Le pouvoir de l'intention est un potentiel de notre esprit, utilisé en réalité par tous les maîtres spirituels, les shamans et les personnes de talent. Le pouvoir de l'intention n'est pas un livre comme les autres. Il révèle des vérités qui changeront radicalement votre vie.",
+        'isbn' => '2-290-35302-7',
+        'pochette' => '9782290353028-475x500-1.jpg',
+        'pages' => 340,
+        'ean' => '9782290353028',
+        'photo_auteur' => 'WayneDyer.jpg'
+    ),
+    array(
+        'auteur' => 'olivier',
+        'titre' => "Entre deux mondes",
+        'resume' => "Adam, policier ayant fui un pays en guerre? a eu le temps de mettre sa femme Nora et sa fille Maya en sécurité avant de les rejoindre. Mais, sur place, Adam ne les retrouve pas et le monde qu’il découvre est effrayant et désolant. Dans cette zone de non-droit, un criminel sévit… Pour sauver un enfant menacé, Adam se lie à Bastien, un policier français… Un polar coup de poing, très documenté sur le sort des réfugiés de Calais.",
+        'pochette' => 'entre-deux-mondes.jpg',
+        'pages' => 352,
+        'ean' => '10118163',
+        'video' => 'olivier.mp4',
+        'photo_auteur' => 'olivierNorek.jpg'
+    ),
+);
