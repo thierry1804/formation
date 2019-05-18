e2e9ac0ccd82d3f17540c8494d1cf5c2d6474214
Thierry <thierry1804@users.noreply.github.com>
2019-05-18 20:52:33 +0300

    Exo

---
+++ exo.php
@@ -0,0 +1,30 @@
+<?php
+//Déclarer une variable $cat (catégorie) et affecter-la la valeur "PC"
+
+//Déclarer une variable $prix et affecter-la la valeur 900
+
+//Déclarer une variable $remise et affecter-la la valeur 0
+
+//Si la catégorie est PC (pour effectuer une comparaison avec =, il faut écrire ==), alors
+
+    //Si le prix est supérieur ou égal à 1000, alors
+  
+        //La remise est 15% du prix
+    
+    //Sinon
+  
+        //La remise est 10% du prix
+    
+//Sinon, si la catégorie est Livre, alors
+
+    //La remise est 5% du prix
+  
+//Sinon
+
+    //La remise est de 2% du prix
+  
+//Afficher la remise
+
+//Déclarer une variable $net (montant net) et affecter-la la différence entre le prix et la remise
+
+//Afficher le montant net
